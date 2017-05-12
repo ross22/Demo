146f726db8e10e92195a046f5c27121e5bc86b98
roushan kumar <roushan kumar>
2017-05-12 19:57:50 +0530

    Initial Draft to node

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./database/database');
+
+// database connection
+mongoose.connect(config.database);
+
+// On Connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database '+config.database);
+});
+
+// On Error
+mongoose.connection.on('error', (err) => {
+  console.log('Database error: '+err);
+});
+
+const app = express();
+
+const users = require('./routes/users');
+
+// Define Port
+const port = 3000;
+
+app.use(cors());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'view')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+// Index Route
+app.get('/', (req, res) => {
+  res.send('Invalid Endpoint');
+});
+
+// Start Server
+app.listen(port, () => {
+  console.log('Server started on port '+port);
+});
